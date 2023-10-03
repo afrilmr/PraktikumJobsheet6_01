27dd1aeb5ac41759323e037d64740f8fa4f906da
afrilmr <144087116+afrilmr@users.noreply.github.com>
2023-10-03 16:00:08 +0700

    Add files via upload

---
+++ Latihan101.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Latihan101 {
+    public static void main(String[] args) {
+
+    Scanner input01 = new Scanner(System.in);
+    int bil1 = 28, bil2 = 54, bil3 = 15;
+
+    if (bil1 >= bil2) {
+        if (bil1 >= bil3) {
+            System.out.println("Bilangan terbesar: " + bil1);
+        } else {
+            System.out.println("Bilangan terbesar: " + bil3);
+        }
+    } else {
+        if (bil2 >= bil3) {
+            System.out.println("Bilangan terbesar: " + bil2);
+        } else {
+            System.out.println("Bilangan terbesar: " + bil3);
+        }
+    }
+    }
+}
