66a26a0b05520df4d784ace9a9737f34ab4916db
afrilmr <144087116+afrilmr@users.noreply.github.com>
2023-10-03 14:06:39 +0700

    Add files via upload

---
+++ Pemilihan2Percobaan201_1.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan201_1 {
+    public static void main(String[] args) {
+
+        Scanner input01 = new Scanner(System.in);
+        float sudut1, sudut2, sudut3, totalSudut;
+             
+        System.out.print("Masukkan Sudut 1 : ");
+        sudut1 = input01.nextFloat();
+        System.out.print("Masukkan Sudut 2 : ");
+        sudut2 = input01.nextFloat();
+        System.out.print("Masukkan Sudut 3 : "); 
+        sudut3 = input01.nextFloat();
+        
+        totalSudut = sudut1 + sudut2 + sudut3;
+    
+        if (totalSudut == 180){
+            if ((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90)){
+                System.out.println("Segitiga tersebut adalah segitiga siku-siku");
+            }else if ((sudut1 == 60) && (sudut2 == 60) && (sudut3 == 60)){
+                System.out.println("Segitiga tersebut adalah segitiga sama sisi");
+            }else if ((sudut1 == sudut2) || (sudut2 == sudut3) || (sudut3 == sudut1)){
+                System.out.println("Segitiga tersebut adalah segitiga sama kaki");
+            }else{
+                System.out.println("Segitiga tersebiut adalah segitiga sembarang");
+            }
+        }else{
+            System.out.println("Bukan Segitiga");
+        }
+    }
+}
